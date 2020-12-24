ea9d76e140f007fec115a76337c3b60b7f1ca901
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-12-24 11:53:13 +0100

    Update userinfo.js

--- Test/commands/userinfo.js
+++ Test/commands/userinfo.js
@@ -9,10 +9,10 @@ module.exports.run = async (client, message, args) => {
     .setImage(message.guild.iconURL)
     .setDescription(`${message.guild}'s Informatie`)
     .addField("Creator", `De Creator Van deze server is ${message.guild.owner}`)
-    .addField("Spelers", `This server has ${message.guild.memberCount} members`)
-    .addField("Emoji Count", `This server has ${message.guild.emojis.cache.size} emojis`)
-    .addField("Rollen", `Deze Server Heeft ${message.guild.roles.cache.map(r => `${r}`).join(" | ")} `)
-    .addField("kanalen", `${message.guild.channels.cache.size}`)
+    .addField("Spelers", `Deze server heeft ${message.guild.memberCount} members`)
+    .addField("Emoji Count", `Deze Server Heeft ${message.guild.emojis.cache.size} emojis`)
+    .addField("Rollen", ` ${message.guild.roles.cache.map(r => `${r}`).join(" | ")} `)
+    .addField("kanalen", `Deze Server heeft ${message.guild.channels.cache.size} Kanalen`)
     
 
 message.channel.send(serverEmbed)
