c38c30f1e9d297dfdd7f6c6077ee11867175ffdf
raymond390 <74500294+raymond390@users.noreply.github.com>
2021-01-08 15:37:21 +0100

    Update sollietest.js

--- Test/commands/sollietest.js
+++ Test/commands/sollietest.js
@@ -166,7 +166,7 @@ module.exports.run = async (client, message, args) => {
                                              .setDescription(`**wat is je roblox naam**: \n${antwoord0}\n\n**hoe oud ben je?**: \n${antwoord1}\n\n**ben je bekend met admin?**: \n${antwoord2}\n\n**wat is AA**: \n${antwoord3}\n\n**wat doe je als je een HR ziet abusen?**:\n${antwoord4}\n\n**wat doe je als je een HC ziet abusen?**: \n${antwoord5}\n\n**wat doe je als er een hacker in de game zit?**: \n${antwoord6}\n\n**welke eenheid wil je? (DSI/Ambulance/politie)**\n${antwoord7}\n\n**wat is je motivatie [50woorden]**\n${antwoord8}\n\n**hoe actief ben jij op een schaal van 1 tot 10?**\n${antwoord9}\n\n**moeten wij nog iets van je weten?**\n${antwoord10}`)
                                          
      
-                                             settedParent.bulkDelete(20).then(
+                                             settedParent.bulkDelete(22).then(
                                                  settedParent.send(uitkomst)
                                              )
                                          })
