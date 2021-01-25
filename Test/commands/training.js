80f8b7cb77f8bc03860a1aa161479e42600818a5
raymond390 <74500294+raymond390@users.noreply.github.com>
2021-01-25 19:02:37 +0100

    Update training.js

--- Test/commands/training.js
+++ Test/commands/training.js
@@ -2,51 +2,21 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    if (!message.member.hasPermission("MANAGE_EMOJIS")) return message.reply("sorry jij kan dit niet");
+    var embedParent = new discord.MessageEmbed()
+    .setTitle(`**Training Inplannen**`)
+    .setColor("#000000")
+    .setDescription(`Hier onder staat het commando uitgelegd
 
-     dienst = args[0];
-     type = args[1];
-     co = args[2];
-     datum = args[3];
-     tijd = args[4];
-     var opmerking = args.slice(5).join(" ");
-   
-    if (!args[0]) return message.reply(" Dientst type co host datum tijd opmerkingen");
-    if (!args[1]) return message.reply(" Dientst type co host datum tijd opmerkingen");
-    if (!args[2]) return message.reply(" Dientst type co host datum tijd opmerkingen");
-    if (!args[3]) return message.reply(" Dientst type co host datum tijd opmerkingen");
-    if (!args[4]) return message.reply(" Dientst type co host datum tijd opmerkingen");
-    if (!args[5]) return message.reply(" Dientst type co host datum tijd opmerkingen");
-   
-   
-    var botEmbed = new discord.MessageEmbed()
-    
-    .setTitle('Training')
-    .setColor("#f00c0c")
-    .setTimestamp()
-    .addFields(
-        {name:" ** Dienst **" , value:(dienst)},
-        {name:" ** Type-Training **" , value: (type) },
-        {name:" ** Host **" , value:` ${message.author} `},
-        {name:" ** Co-Host **" , value: (co) },
-        {name:" ** Datum **" , value: (datum) },
-        {name:" ** Tijd **" , value: (tijd) },
-        {name:" ** Opmerking **" , value:` ${opmerking}`},
-                
-    )
-    .setThumbnail('')
-    .setImage('')
-    .setFooter('', '');
+    .training <TYPE> <CO-HOST> <SUPERVISOR> <DATUM> <TIJD> <OPMERKINGEN>`)
+    .setThumbnail("")
+    .setFooter('', '')
 
-    var channel = message.member.guild.channels.cache.get("784430428722298941");
+settedParent.send(embedParent);
 
-    if (!channel) return
-
-    channel.send(botEmbed);
 }
 
 module.exports.help = {
     name: "training",
-    description: ".training Dientst type co host datum tijd opmerkingen",
-    category: "Algemeen"
+    description: "Geeft al de verschillende commands",
+    category: "Informatie"
 }
