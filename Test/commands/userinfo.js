43d608db7f037519abff5b62ed60c255036874f4
raymond390 <74500294+raymond390@users.noreply.github.com>
2021-01-26 15:01:49 +0100

    s

--- Test/commands/userinfo.js
+++ Test/commands/userinfo.js
@@ -2,25 +2,48 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-  
-    var serverEmbed = new discord.MessageEmbed()
-    .setColor("RANDOM")
-    .setTitle("Server Info")
-    .setImage(message.guild.iconURL)
-    .setDescription(`${message.guild}'s Informatie`)
-    .addField("Creator", `De Creator Van deze server is ${message.guild.owner}`)
-    .addField("Spelers", `Deze server heeft ${message.guild.memberCount} members`)
-    .addField("Emoji Count", `Deze Server Heeft ${message.guild.emojis.cache.size} emojis`)
-    .addField("Rollen", ` ${message.guild.roles.cache.map(r => `${r}`).join(" | ")} `)
-    .addField("kanalen", `Deze Server heeft ${message.guild.channels.cache.size} Kanalen`)
+        let userinfo = {};
+        userinfo.bot = message.client.user.bot;
+        userinfo.createdat = message.client.user.createdAt;
+        userinfo.discrim = message.client.user.discriminator;
+        userinfo.id = message.client.user.id;
+        userinfo.mfa = message.client.user.mfaEnabled;
+        userinfo.pre = message.client.user.premium;
+        userinfo.presen = message.client.user.presence;
+        userinfo.tag = message.client.user.tag;
+        userinfo.uname = message.client.user.username;
+        userinfo.verified = message.client.user.verified;
+    
+        userinfo.avatar = message.client.user.avatarURL;
+    
+        var myInfo = new discord.RichEmbed()
+            .setAuthor(userinfo.uname, userinfo.avatar)
+            .addField("Bot?",userinfo.bot, true)
+            .addField("Created At",userinfo.createdat, true)
+            .addField("Discriminator",userinfo.discrim, true)
+            .addField("Client ID",userinfo.id, true)
+            .addField("2FA/MFA Enabled?",userinfo.mfa, true)
+            .addField("Paid Account?",userinfo.pre, true)
+            .addField("Presence",userinfo.presen, true)
+            .addField("Client Tag",userinfo.tag, true)
+            .addField("Username",userinfo.uname, true)
+            .addField("Verified?",userinfo.verified, true)
+            .setColor(0xf0e5da)
+            .setFooter('s!aboutserver')
+            .setTitle("About this user...")
+            .setThumbnail(userinfo.avatar)
+    
+    
+            message.channel.sendEmbed(myInfo);
+    
+    }
+    
     
 
-message.channel.send(serverEmbed)
-  
+
+
+module.exports.help = {
+    name: "userinfo",
+    description: "Geeft al de verschillende commands",
+    category: "Informatie"
 }
-    
-    module.exports.help = {
-        name: "serverinfo",
-        description: "Geeft al de verschillende commands",
-        category: "Informatie"
-    }
