35caeb089ed542d6e46337acec73e4df8c8a3cd9
raymond390 <74500294+raymond390@users.noreply.github.com>
2021-01-29 15:47:41 +0100

    Update sollietest.js

--- Test/commands/sollietest.js
+++ Test/commands/sollietest.js
@@ -11,29 +11,23 @@ module.exports.run = async (client, message, args) => {
         var userName = message.author.username;
         var userDiscriminator = message.author.discriminator;
     
-        var ticketBestaat = true;
+        var ticketBestaat = false;
     
         message.guild.channels.cache.forEach(channel => {
     
-            if (channel.name == userName.toLowerCase()) {
-                ticketBestaat = true;
-    
-                message.reply("Je hebt al een ticket aangemaakt");
-    
-                return;
-            }
+        
     
         });
     
         if (ticketBestaat) return;
     
         var embed = new discord.MessageEmbed()
-            .setTitle(`Hoi  ${message.author} `)
+            .setTitle("Hoi " + message.author.username)
             .setFooter("Sollicitatie kanaal wordt aangemaakt");
     
         message.channel.send(embed);
     
-        message.guild.channels.create(userName.toLowerCase(), { type: 'text' }).then(
+        message.guild.channels.create(userName.toLowerCase() + "-" + userDiscriminator, { type: 'text' }).then(
             (createdChannel) => {
                 createdChannel.setParent(categoryID).then(
                     (settedParent) => {
