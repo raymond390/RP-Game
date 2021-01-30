ba18f1fd97cfd7136b67c120d4bbdb800020b289
raymond390 <74500294+raymond390@users.noreply.github.com>
2021-01-30 12:29:48 +0100

    Update economy.js

--- Test/features/economy.js
+++ Test/features/economy.js
@@ -1,5 +1,5 @@
 const mongo = require('../mongo')
-const profileSchema = require('./schemas/profile-schema')
+const profileSchema = require('../schemas/profile-schema')
 
 const coinsCache = {} // { 'guildId-userId': coins }
 
