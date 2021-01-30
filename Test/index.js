5a5fcf0124a4f2c4f72266c41f29f02c270b4301
raymond390 <74500294+raymond390@users.noreply.github.com>
2021-01-30 11:17:35 +0100

    Update index.js

--- Test/index.js
+++ Test/index.js
@@ -7,7 +7,7 @@ const messageCount = require('./features/message-counter')
 const activeSongs = new Map();
 const poll = require('./features/poll')
 const scaling = require('./features/scaling-channels')
-
+const path = require('path')
 
 //  Command handler
 const fs = require("fs");
