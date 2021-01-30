ef01014502a10baa6ae278bc61a3fe1c6e7a3462
raymond390 <74500294+raymond390@users.noreply.github.com>
2021-01-30 11:18:58 +0100

    Update command-base.js

--- Test/command/command-base.js
+++ Test/command/command-base.js
@@ -66,7 +66,7 @@ module.exports = (client, commandOptions) => {
     commands = [commands]
   }
 
-  console.log(`Registering command "${commands[0]}"`)
+
 
   // Ensure the permissions are in an array and are all valid
   if (permissions.length) {
