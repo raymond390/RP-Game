528db69881cdc2eeaa9d5ffac29f55d10cd8812b
raymond390 <74500294+raymond390@users.noreply.github.com>
2021-01-30 11:25:34 +0100

    Update command-base.js

--- Test/command/command-base.js
+++ Test/command/command-base.js
@@ -81,7 +81,7 @@ module.exports = (client, commandOptions) => {
   client.on('message', (message) => {
     const { member, content, guild } = message
 
-    for (const alias of command) {
+    for (const of alias) {
       const command = `${prefix}${alias.toLowerCase()}`
 
       if (
