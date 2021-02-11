bdd2305cc2f5d0e393383cc10e40de79764634ac
raymond390 <74500294+raymond390@users.noreply.github.com>
2021-02-11 16:24:42 +0100

    Create bugs.js

---
+++ Test/command/bugs.js
@@ -0,0 +1,40 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+ 
+   
+    var opmerking = args.slice(0).join(" ");
+    const user = message.mentions.users.first() || message.member.user
+
+    if (!args[0]) return message.reply("Geen suggestie gegeven");
+    
+
+        // Create embed.
+        var embedCreateTicket = new discord.MessageEmbed()
+            .setAuthor(`${user.username}`, user.displayAvatarURL())
+            .setTitle('🚨 Bugreport')
+            .setDescription(`${opmerking}`)
+            .setFooter(`Bug ${user.username}`)
+            .setColor("#ed0909");
+            
+            
+            
+
+        // Channel voor logging
+        var ticketChannel = message.member.guild.channels.cache.find(channel => channel.name === "bugs");
+
+        ticketChannel.send(embedCreateTicket)
+   
+        
+
+
+    
+
+
+}
+
+module.exports.help = {
+    name: "bug",
+    category: "Informatie",
+    description: "Geef een suggestie",
+}
